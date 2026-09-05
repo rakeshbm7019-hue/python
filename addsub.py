c743f9766d02b6b5ee38d3a247201db93545fa11
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-09-05 19:10:46 +0530

    commit

---
+++ addsub.py
@@ -0,0 +1,5 @@
+a, b = 10, 5
+print("Addition:", a + b)
+
+
+print("Subtraction:", a - b)
---
+++ fibanoci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    a, b = 0, 1
+    series = []
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+    return series
+
+print("Fibonacci (first 10):", fibonacci(10))
